8c87face1e926911ca15d6415a2aad02b607efbc
Mauricio Cubides <mcubides@javeriana.edu.co>
2024-10-09 15:15:27 -0500

    Clase 10 y 11 - Firebase

---
+++ android-nativo/Clases10-11-Firebase/app/src/main/java/com/icm2430/firebase/model/Address.kt
@@ -0,0 +1,8 @@
+package com.icm2430.firebase.model
+
+class Address {
+    var city: String? = null
+    var address: String? = null
+
+    constructor() {}
+}
---
+++ android-nativo/Clases10-11-Firebase/app/src/main/java/com/icm2430/firebase/model/DatabasePaths.kt
@@ -0,0 +1,6 @@
+package com.icm2430.firebase.model
+
+object DatabasePaths {
+    const val PEOPLE = "people/"
+    const val CHAT = "chat/"
+}
